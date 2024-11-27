6f20ea8f6a11df47fc2c662bcb5ed3f42eb1e5cd
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-11-27 16:29:01 +0700

    add ui

--- templates/assets/js/pages/chat.js
+++ templates/assets/js/pages/chat.js
@@ -394,9 +394,8 @@ class Contact{
     constructor(){
         this.btnOpenFormAddContact = document.querySelector('.btn.btn-soft-primary.btn-sm');
         this.addContactModal = document.querySelector('#addContact-exampleModal');
-        this.emailInput = this.addContactModal.querySelector('.find-user');
+        this.emailInput = this.addContactModal.querySelector('#addcontactemail-input');
         this.btnSearchContact = this.addContactModal.querySelector('.modal-footer .btn.btn-primary');
-        // this.btnSearchContact.onclick = ()=>alert('Searching')
 
 
         this.contactDiv = document.querySelector('.sort-contact');
@@ -458,6 +457,7 @@ class Contact{
     }
     async init(){
         const contacts = await this.getContacts()
+        console.log(contacts);
         this.renderContacts(contacts);
         this.btnAddMoreContact.onclick = ()=>{
             this.openFormAddMoreContactModel();
@@ -471,18 +471,25 @@ class Contact{
         search.addEventListener('input',(e)=>{
             this.seachUnjoinedContact(e)
         })
+
+        this.btnSearchContact.onclick = ()=>{
+            this.findContactByEmail();
+        }
+
     }
 
     renderContacts(contacts){
+        console.log(contacts)
         this.contactDiv.innerHTML = ''
         const ul = document.createElement('ul');
         ul.className = 'list-unstyled';
+        let image_path = 'https://s120-ava-talk.zadn.vn/4/8/8/f/4/120/e39bbde1f51d8b1bac7f79ce4510bd7d.jpg';
         for(let contact of contacts){
             const li = document.createElement('li');
             li.className = 'contact';
             li.innerHTML = `
-                <img src = ${contact.image_path}  style = 'width:2.4rem; height:2.4rem; border-radius:50%!important;'>
-                <span class = 'name' style= 'font-size:medium; font-weight:600;' >${contact.name}</span>
+                <img src = ${image_path}  style = 'width:2.4rem; height:2.4rem; border-radius:50%!important;'>
+                <span class = 'name' style= 'font-size:medium; font-weight:600;' >${contact.username}</span>
             `
             li.onclick = ()=>this.selectContact(contact);
             ul.appendChild(li);
@@ -494,8 +501,9 @@ class Contact{
         store.setState({ selectedContact: contact });
     }
     async getContacts(){
+        // alert('a')
         let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVkQXQiOjE3MzMyOTg4MzksInVzZXJfaWQiOiIyIn0.srJB58MbZbN76nxOw3QEPq2-xJkw60Grl9dtugo_EOM'
-        fetch('/api/v1/contacts',{
+        return fetch('/api/v1/contacts',{
             method:'GET',
             headers:{
                 'content-type':'application/json',
@@ -504,41 +512,55 @@ class Contact{
         }).then(async response => {
             if(!response.ok)return;
             const contact = await response.json()
-            return contact;
+            // console.log(contact.contacts);
+            return contact.contacts;
         })
-        // const contacts = [
-        //     {
-        //         id: 1,
-        //         email: 'habc',
-        //         image_path:'assets/images/users/avatar-1.jpg',
-        //         name:'Nguyễn An',
-        //     },
-        //     {
-        //         id: 2,
-        //         email: 'habc',
-        //         image_path:'assets/images/users/avatar-1.jpg',
-        //         name:'Nguyễn thị bảo',
-        //     },
-        //     {
-        //         id: 3,
-        //         email: 'habc',
-        //         image_path:'https://s120-ava-talk.zadn.vn/4/8/8/f/4/120/e39bbde1f51d8b1bac7f79ce4510bd7d.jpg',
-        //         name:'Tấn',
-        //     },
-        //     {
-        //         id: 10,
-        //         email: 'habc',
-        //         image_path:'https://s120-ava-talk.zadn.vn/4/8/8/f/4/120/e39bbde1f51d8b1bac7f79ce4510bd7d.jpg',
-        //         name:'Chưa nhắn lần nào',
-        //     },
-
-        // ]
-        // return contacts;
     }
     async getUnJoinContactOfGroup(){
         return this.getContacts();//tạm
     }
+    findContactByEmail(){
+        const email = this.emailInput.value;
 
+        //console.log(email);
+    }
+    createFindedContact(contactInfo){
+        const contact = document.createElement('div');
+        contact.innerHTML = `
+           <div class="user" style="display: flex; justify-content: space-between; align-items: center;">
+            <div style="display: flex; gap:20px;">
+                <img src="assets/images/users/avatar-2.jpg" alt="" style="width: 35px; height: 35px; border-radius: 50%;">
+                <div>
+                    <p style="margin: 0;">${contactInfo.username}</p>
+                    <p style="margin: 0;">${contactInfo.email}</p>
+                </div>
+            </div>
+            <div>
+                    <span class="btn btn-add-contact">
+                        <svg fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             viewBox="0 0 78.509 78.509" xml:space="preserve">
+                            <g>
+                                <path d="M68.305,51.149h-3.032v-3.031v-6h-6h-5.281c-2.281-2.832-4.785-5.04-7.03-6.974c3.829-3.723,6.22-8.918,6.22-14.668
+                                    C53.182,9.186,43.996,0,32.706,0S12.23,9.186,12.23,20.476c0,5.75,2.39,10.945,6.219,14.668
+                                    C12.318,40.425,4.205,47.729,4.205,64.26v3h33.708v2.218h6h3.033v3.031v6h6h6.326h6v-6v-3.031h3.032h6v-6V57.15v-6L68.305,51.149
+                                    L68.305,51.149z M18.23,20.476C18.23,12.494,24.724,6,32.706,6c7.981,0,14.476,6.494,14.476,14.476
+                                    c0,7.449-5.656,13.597-12.897,14.386c-0.072,0.007-0.143,0.016-0.215,0.021c-0.347,0.033-0.698,0.046-1.051,0.054
+                                    c-0.097,0.002-0.192,0.01-0.289,0.011c-0.153-0.001-0.303-0.012-0.455-0.017c-0.292-0.009-0.584-0.018-0.871-0.044
+                                    c-0.108-0.008-0.215-0.021-0.322-0.031C23.862,34.044,18.23,27.908,18.23,20.476z M22.736,39.369
+                                    c0.158-0.137,0.315-0.271,0.472-0.406c2.29,0.981,4.736,1.607,7.242,1.858c0.176,0.02,0.355,0.028,0.532,0.044
+                                    c0.469,0.036,0.939,0.062,1.411,0.071c0.105,0.001,0.207,0.016,0.312,0.016c0.078,0,0.154-0.011,0.231-0.012
+                                    c0.523-0.009,1.045-0.037,1.566-0.079c0.143-0.013,0.287-0.021,0.428-0.036c2.505-0.246,4.965-0.874,7.271-1.862
+                                    c0.155,0.135,0.313,0.27,0.472,0.406c1.415,1.217,2.872,2.48,4.272,3.887v4.862v3.031h-3.033h-6v6v4.11h-27.6
+                                    C11.187,49.303,17.297,44.047,22.736,39.369z M68.305,63.478h-9.032v9.031h-6.326v-9.031h-9.033V57.15h9.033v-9.031h6.326v9.031
+                                    h9.032V63.478z"/>
+                            </g>
+                        </svg>
+                    </span>
+            </div>
+        </div>
+        `
+        return contact;
+    }
 }
 class Profile{
 
